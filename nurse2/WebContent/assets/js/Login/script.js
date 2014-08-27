7b6af6686530dd75a4f687b9ff14f3d67fb33f6a
Sung Kyu <sunq0011@gmail.com>
2014-08-27 13:41:11 +0900

    rolend 2014-08-27

--- nurse2/WebContent/assets/js/Login/script.js
+++
@@ -1,7 +0,0 @@
-function memIdCheck(){ // 회원가입 페이지의 중복체크 페이지 연결
-	 window.open("memIdCheck.do","post","width=600, height=600, toolbar=no");
- }
- function memZipCheck(){ // 회원가입 페이지의 우편번호 검색 페이지 연결
-	 var url = "memZipCheck.do?check=false";
-	 window.open(url,"post","width=600, height=600, directories=no, status=yes, scrollbars=yes, menubar=no");
- }
